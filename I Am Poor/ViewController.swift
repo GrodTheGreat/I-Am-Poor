2082ac0914a411075207c04d7539edb57c12d279
GrodTheGreat <60365291+GrodTheGreat@users.noreply.github.com>
2024-12-24 23:55:23 -0500

    Add files via upload
    Complete Section

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Gerard  Causse on 12/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
